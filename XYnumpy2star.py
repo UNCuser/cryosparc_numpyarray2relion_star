81e6831d5de363983355435f363f2f9c31878863
Jonathan Fay <fayj@unc.edu>
2021-05-04 09:21:52 -0400

    Update XYnumpy2star.py

--- XYnumpy2star.py
+++ XYnumpy2star.py
@@ -25,6 +25,8 @@ out_ext = ".star"
 particles = cs['blob/path']
 X = cs['location/center_x_frac']
 Y = cs['location/center_y_frac']
+#y flip
+#Y = 1 - cs['location/center_y_frac']
 
 particle_locs = {}
 for j in range(len(particles)):
@@ -32,7 +34,7 @@ for j in range(len(particles)):
 
 for j in range(len(particles)):
 	particle_locs[str(particles[j])].append([X[j]*float(x_norm), Y[j]*float(y_norm)])
-
+	
 stdout_ref = sys.stdout
 for parti in particles:
 	noSlashes = parti.split("/")
